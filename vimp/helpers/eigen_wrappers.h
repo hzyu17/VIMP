d8f5ff7c6d533f52412d5d7a07d9095ba215c9b7
lucasyu17 <hz17.yu@gmail.com>
2023-01-06 12:43:45 -0500

    eigen wrappers half way

--- vimp/helpers/eigen_wrappers.h
+++ vimp/helpers/eigen_wrappers.h
@@ -1,7 +1,7 @@
 /**
  * @file eigen_wrappers.h
  * @author Hongzhe Yu (hyu419@gatech.edu)
- * @brief Wrappers for Eigen libraries
+ * @brief Wrappers for Eigen libraries.
  * @version 0.1
  * @date 2023-01-05
  * 
@@ -11,30 +11,46 @@
 
 #include<Eigen/Dense>
 #include<Eigen/Sparse>
+#include<Eigen/SparseCholesky>
 #include"random.h"
 
 typedef Eigen::VectorXd Vector;
+typedef Eigen::VectorXcd VectorC;
 typedef Eigen::MatrixXd Matrix;
 typedef Eigen::SparseMatrix<double> SpMat; // declares a column-major sparse matrix type of double
+typedef Eigen::SparseVector<double> SpVec; 
 typedef Eigen::Triplet<double> T;
 
 class MatrixClass{
 public:
     MatrixClass(){};
+
+    // ================= min and max values in a matrix or a vector =================
+    double minval(const Matrix& m){
+        return m.minCoeff();
+    }
+
+    double maxval(const Matrix& m){
+        return m.maxCoeff();
+    }
     
+    // ================= random matrix, full and sparse =================
     Matrix random_matrix(int m, int n){
         return Matrix::Random(m ,n);
     }
 
     SpMat random_sparse_matrix(int m, int n, int nnz){
+        if (nnz > m*n){
+            throw std::invalid_argument( "received negative value" );
+        }
         std::vector<T> tripletList;
         tripletList.reserve(nnz);
 
         Random random;
 
         for (int i=0; i<nnz; i++){
-            int i_row = random.randint(0, m);
-            int j_col = random.randint(0, n);
+            int i_row = random.randint(0, m-1);
+            int j_col = random.randint(0, n-1);
             double val = random.rand_double(0.0, 10.0);
             tripletList.push_back(T(i_row, j_col, val));
         }
@@ -44,4 +60,66 @@ public:
 
         return mat;
     }
+
+    Vector random_vector(int n){
+        return Vector::Random(n);
+    }
+
+
+    // ================= Eigen valules and eigen vectors =================
+    VectorC eigen_values(const Matrix& m){
+        return m.eigenvalues();
+    }
+
+    // return real part of eigen values.
+    Vector real_eigenvalues(const Matrix& m){
+        VectorC eigen_vals{eigen_values(m)};
+        return eigen_vals.real();
+    }
+
+
+    // ================= PSD matrix =================
+    Matrix random_psd(int n){
+        Matrix m{random_matrix(n, n)};
+        return m.transpose() * m;
+    }
+
+    /**
+     * @brief sparse psd matrix. P = A^T*A.
+     * 
+     * @param n : size of matrix P.
+     * @param nnz : number of non-zeors in A.
+     * @return SpMat 
+     */
+    SpMat randomd_sparse_psd(int n, int nnz){
+        SpMat A{random_sparse_matrix(n, n, nnz)};        
+        return A.transpose() * A;
+    }
+
+    bool is_sparse_positive(const SpMat& spm){
+        Matrix m{spm};
+        Eigen::LDLT<Matrix> ldlt(m);        
+        return ldlt.isPositive();
+    }
+
+    bool is_positive(const Matrix& m){
+        Eigen::LDLT<Matrix> ldlt(m);
+        return ldlt.isPositive();
+    }
+
+    // ================= IO for matrix =================
+    void print_matrix(const Matrix& m){  
+        Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+        std::cout << m.format(CleanFmt) << _sep;
+    }
+
+    void print_spmatrix(const SpMat& sp_m){
+        Matrix m(sp_m);
+        Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+        std::cout << m.format(CleanFmt) << _sep;
+    }
+
+private:
+    // IO related
+    std::string _sep = "\n----------------------------------------\n";
 };
--- vimp/tests/test_eigen_sparse.cpp
+++ vimp/tests/test_eigen_sparse.cpp
@@ -15,10 +15,15 @@
 #include"../helpers/timer.h"
 #include"../helpers/eigen_wrappers.h"
 
+#include<Eigen/SparseCholesky>
+#include<Eigen/IterativeLinearSolvers>
+
+
 typedef Eigen::VectorXd Vector;
 typedef Eigen::MatrixXd Matrix;
 typedef Eigen::SparseMatrix<double> SpMat; // declares a column-major sparse matrix type of double
 typedef Eigen::Triplet<double> T;
+
 
 /**
  * @brief Test initialization of a sparse matrix.
@@ -27,18 +32,97 @@ TEST(TestSparse, initialization){
     int m = 4; // number of rows and cols
     int n = m*m; // the matrix dimension
 
-    MatrixClass matrix_class;
+    MatrixClass m_class;
 
     Timer timer;
 
     timer.start();
-    
-    Matrix rand_mat = matrix_class.random_matrix(n, n);
-    SpMat rand_spmat = matrix_class.random_sparse_matrix(n, n, 4);
+    Matrix rand_mat = m_class.random_matrix(n, n);
+    // m_class.print_matrix(rand_mat);
+    std::cout << "full matrix init" << std::endl;
+    timer.end();
 
+    timer.start();
+    SpMat rand_spmat = m_class.random_sparse_matrix(n, n, 10);
+    // m_class.print_spmatrix(rand_spmat);
+    std::cout << "sparse matrix init" << std::endl;
     timer.end();
 
 }
+
+
+TEST(TestSparse, computation_time){
+    Timer timer;
+
+    int m=4;
+    int n=m*m;
+
+    MatrixClass m_class;
+
+    SpMat spm1 = m_class.random_sparse_matrix(n, n, 4);
+    SpMat spm2 = m_class.random_sparse_matrix(n, n, 4);
+    SpMat spm3 = m_class.random_sparse_matrix(n, n, 4);
+
+    Matrix m1(spm1);
+    Matrix m2(spm2);
+    Matrix m3(n, n);
+
+    timer.start();
+    spm3 = spm1 * spm2;
+    std::cout << "sparse multiplication " << std::endl;
+    timer.end();
+    
+    timer.start();
+    m3 = m1 * m2;
+    std::cout << "full multiplication " << std::endl;
+    timer.end();
+}
+
+TEST(TestSparse, solve_psd_eqn){
+    Timer timer;
+
+    int m=4;
+    int n=m*m;
+
+    MatrixClass m_class;
+
+    SpMat spm = m_class.randomd_sparse_psd(n, 10);
+    m_class.print_spmatrix(spm);
+
+    ASSERT_TRUE(m_class.is_sparse_positive(spm));
+
+    // RHS
+    Vector b(m_class.random_vector(n));
+
+    // solve full
+    Matrix fullm(spm);
+    timer.start();
+    Vector x = fullm.llt().solve(b);
+    std::cout << "solving time full matrix " << std::endl;
+    timer.end();
+
+    m_class.print_matrix(x);
+
+    // solve sparse
+    timer.start();
+    Eigen::ConjugateGradient<SpMat, Eigen::Upper> solver;
+    Vector spx = solver.compute(spm).solve(b);
+
+    // Eigen::SimplicialLLT<SpMat> sp_llt(spm);
+    // Vector sp_x = sp_llt.solve(b);
+    std::cout << "solving time sparse matrix " << std::endl;
+    timer.end();
+
+    m_class.print_matrix(spx);
+
+    ASSERT_LE((x - spx).norm(), 1e-7);
+
+}
+
+TEST(TestSparse, ldlt_decomp){
+    ASSERT_TRUE(true);
+}
+
 
 int main(int argc, char **argv){
 
