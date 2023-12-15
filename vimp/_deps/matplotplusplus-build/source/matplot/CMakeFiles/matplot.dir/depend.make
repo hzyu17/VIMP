9ce6fe68ffb468096438a13ae696ab3f7fdd8c81
hzyu17 <hz17.yu@gmail.com>
2023-12-15 04:40:03 -0500

    clean makefiles

--- vimp/_deps/matplotplusplus-build/source/matplot/CMakeFiles/matplot.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for matplot.
-# This may be replaced when dependencies are built.
--- vimp/_deps/matplotplusplus-build/source/matplot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hzyu/git/VIMP/vimp/_deps/matplotplusplus-src/source/matplot
-# Build directory: /home/hzyu/git/VIMP/vimp/_deps/matplotplusplus-build/source/matplot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
