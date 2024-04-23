3a2235dceb2dd0b4d25c28363caa4e74303c02e9
hzyu17 <hz17.yu@gmail.com>
2024-04-22 21:16:51 -0400

    rm matlab folder from main

--- matlab_helpers/tools/gtsam_toolbox/+gtsam/PrintKeyList.m
+++
@@ -1,8 +0,0 @@
-function varargout = PrintKeyList(varargin)
-      if length(varargin) == 1 && isa(varargin{1},'gtsam.KeyList')
-        gtsam_wrapper(2834, varargin{:});
-      elseif length(varargin) == 2 && isa(varargin{1},'gtsam.KeyList') && isa(varargin{2},'char')
-        gtsam_wrapper(2835, varargin{:});
-      else
-        error('Arguments do not match any overload of function gtsam.PrintKeyList');
-      end
