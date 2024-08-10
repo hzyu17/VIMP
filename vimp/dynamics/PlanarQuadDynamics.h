95ca91b9e690e3657541a6005ceab9cf074f8426
hzyu17 <hz17.yu@gmail.com>
2024-08-10 08:38:03 -0400

    updates

--- vimp/dynamics/PlanarQuadDynamics.h
+++ vimp/dynamics/PlanarQuadDynamics.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cmath>
 #include <Eigen/Dense>
+#include <Eigen/CXX11/Tensor>
 
 using namespace Eigen;
 
@@ -47,6 +48,46 @@ std::tuple<std::vector<MatrixXd>, std::vector<MatrixXd>, std::vector<VectorXd>> 
     }
 
     return std::make_tuple(hA, hB, ha);
+}
+
+/**
+ * @brief The forward kinematics to compute the locations of the collision-checking balls.
+ * 
+ * @param x : states (half): [px, pz, \phi]
+ * @param L : planar quad length
+ * @param n_balls : number of collision checking balls
+ * @param radius : radius
+ * @return std::tuple<Eigen::MatrixXd, Eigen::Tensor<double, 3>, Eigen::VectorXd> 
+ */
+std::tuple<Eigen::MatrixXd, Eigen::Tensor<double, 3>, Eigen::VectorXd>
+vec_balls(const Eigen::VectorXd& x, double L, int n_balls, double radius) {
+    int nx = x.size();
+    Eigen::MatrixXd v_pts = Eigen::MatrixXd::Zero(n_balls, 2);
+    Eigen::Tensor<double, 3> v_gradient_ball_states(n_balls, 2, nx);
+    v_gradient_ball_states.setZero();
+    Eigen::VectorXd v_radius = Eigen::VectorXd::Constant(n_balls, radius);
+
+    double pos_x = x(0);
+    double pos_z = x(1);
+    double phi = x(2);
+
+    double l_pt_x = pos_x - (L - radius * 1.5) * std::cos(phi) / 2.0;
+    double l_pt_z = pos_z - (L - radius * 1.5) * std::sin(phi) / 2.0;
+
+    for (int i = 0; i < n_balls; ++i) {
+        double pt_xi = l_pt_x + L * std::cos(phi) / n_balls * i;
+        double pt_zi = l_pt_z + L * std::sin(phi) / n_balls * i;
+        v_pts(i, 0) = pt_xi;
+        v_pts(i, 1) = pt_zi;
+
+        v_gradient_ball_states(i, 0, 0) = 1.0;
+        v_gradient_ball_states(i, 0, 2) = (L - radius * 1.5) * std::sin(phi) / 2.0 - L * std::sin(phi) / n_balls * i;
+
+        v_gradient_ball_states(i, 1, 1) = 1.0;
+        v_gradient_ball_states(i, 1, 2) = -(L - radius * 1.5) * std::cos(phi) / 2.0 + L * std::cos(phi) / n_balls * i;
+    }
+
+    return std::make_tuple(v_pts, v_gradient_ball_states, v_radius);
 }
 
 int main() {
