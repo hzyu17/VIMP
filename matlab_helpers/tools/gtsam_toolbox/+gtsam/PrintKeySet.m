412f7701d4f0ec8bd68ed73d1b65e85f0ba88219
zinuo <benjaminchang0731@gmail.com>
2024-10-30 17:00:50 -0400

    Update

--- matlab_helpers/tools/gtsam_toolbox/+gtsam/PrintKeySet.m
+++
@@ -1,8 +0,0 @@
-function varargout = PrintKeySet(varargin)
-      if length(varargin) == 1 && isa(varargin{1},'gtsam.KeySet')
-        gtsam_wrapper(2836, varargin{:});
-      elseif length(varargin) == 2 && isa(varargin{1},'gtsam.KeySet') && isa(varargin{2},'char')
-        gtsam_wrapper(2837, varargin{:});
-      else
-        error('Arguments do not match any overload of function gtsam.PrintKeySet');
-      end
