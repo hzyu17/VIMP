aef11dc587333f1e878c92fb6cb7468e2dc92966
ctaylor319 <ctaylor319@gatech.edu>
2024-11-21 00:53:47 -0500

    Fixed default arch_sm value in CMake

--- build_GVIMP_modules.py
+++ build_GVIMP_modules.py
@@ -16,10 +16,16 @@ EIGEN_BUILD_DIR = EIGEN_DIR+"/build"
 CUDA_DIR = DEP_DIR+"/cuda"
 DEVICE_QUERY_DIR = CUDA_DIR+"/cuda-samples/Samples/1_Utilities/deviceQuery"
 
+print("NOTE: GVIMP will only build correctly if you have already installed MATLAB Runtime Library R2020b")
+input("Press enter to continue... ")
+time.sleep(0.5)
+
+print("Cloning GaussianVI subdirectory... ")
+time.sleep(2)
 os.chdir(vimp_DIR)
 os.system("git clone git@github.com:hzyu17/GaussianVI.git")
 
-print("Downloading Eigen-3.4.0.")
+print("Downloading Eigen-3.4.0... ")
 print("NOTE: This will overwrite any Eigen version currently on your machine.")
 input("Press enter to continue... ")
 os.chdir(VIMP_DIR)
@@ -29,15 +35,16 @@ os.chdir(EIGEN_BUILD_DIR)
 os.system("cmake ..")
 os.system("sudo make install")
 
-print("Downloading OpenMP.")
+print("Downloading OpenMP... ")
 time.sleep(2)
 os.chdir(VIMP_DIR)
 os.system("sudo apt-get install -y --ignore-missing libomp-dev")
 
-print("Installing CUDA. Note that this requires a compatible nvidia GPU.")
+print("Installing CUDA... ")
+print("NOTE: This step requires a compatible nvidia GPU.")
+print("NOTE: Assuming an x86_64 CPU architecture.")
 time.sleep(2)
 os.system("mkdir dependencies")
-print("Assuming an x86_64 CPU architecture.")
 time.sleep(0.5)
 os.chdir(DEP_DIR)
 os.system("mkdir cuda")
@@ -51,7 +58,7 @@ os.system("sudo apt-get update")
 os.system("sudo apt-get install -y cuda-toolkit-12-6")
 os.system("sudo apt-get install -y nvidia-driver-535")
 
-print("Adding CUDA path to environmental variables")
+print("Adding CUDA path to environmental variables... ")
 time.sleep(2)
 os.chdir(HOME_DIR)
 bashrc_file = open(".bashrc", "r")
@@ -67,7 +74,7 @@ else:
 bashrc_file.close()
 os.system("source .bashrc")
 
-print("Checking system GPU capability...")
+print("Checking system GPU capability... ")
 time.sleep(2)
 os.chdir(CUDA_DIR)
 os.system("git clone https://github.com/NVIDIA/cuda-samples.git")
@@ -78,7 +85,7 @@ os.system("./deviceQuery > output.txt")
 
 file = open("output.txt", "r")
 
-print("Updating GPU capability in CMake")
+print("Updating CMake as necessary... ")
 time.sleep(2)
 for line in file:
     if line.find("CUDA Capability") >= 0:
@@ -95,7 +102,7 @@ for line in GVI_file_contents:
             replace = line.replace("/usr/local/MATLAB/R2020b", "/usr/local/MATLAB/MATLAB_Runtime/v99")
             GVI_file_contents[line_num] = replace
     if "set(CUDA_ARCH" in line:
-        replace = line.replace("sm_81", arch_sm)
+        replace = line.replace("sm_86", arch_sm)
         GVI_file_contents[line_num] = replace
     line_num += 1
 GVI_file.seek(0)
@@ -103,7 +110,7 @@ GVI_file.truncate(0)
 GVI_file.writelines(GVI_file_contents)
 GVI_file.close()
 
-print("Building VIMP")
+print("Building VIMP... ")
 time.sleep(2)
 os.chdir(VIMP_DIR)
 os.system("mkdir build")
@@ -111,7 +118,7 @@ os.chdir(VIMP_BUILD_DIR)
 os.system("cmake ..")
 os.system("sudo make install")
 
-print("Creating/Saving GH weights")
+print("Creating/Saving GH weights... ")
 time.sleep(2)
 os.system("./src/generate_sigmapts")
 os.system("./src/save_SparseGH_weights")
