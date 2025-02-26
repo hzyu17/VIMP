457469dbe1e453ab54b981dc1ab1756d754b4a2c
zinuo <benjaminchang0731@gmail.com>
2025-02-26 16:02:16 -0500

    Update Arm Robot

--- vimp/maps/WAM/WAMSDF3D_cereal.m
+++ vimp/maps/WAM/WAMSDF3D_cereal.m
@@ -3,15 +3,18 @@
 close all
 clear
 
-import gtsam.*
-import gpmp2.*
-addpath("/usr/local/gtsam_toolbox")
-addpath("../../../matlab_helpers/tools")        
+% import gtsam.*
+% import gpmp2.*
+% addpath("/usr/local/gtsam_toolbox")
+% addpath("../../../matlab_helpers/tools")      
+
+addpath("../../../matlab_helpers/tools")
+addpath("..")
 
 %% dataset
 dataset = generate3Ddataset_1('WAMDeskDataset');
 origin = [dataset.origin_x, dataset.origin_y, dataset.origin_z];
-origin_point3 = Point3(origin');
+% origin_point3 = Point3(origin');
 cell_size = dataset.cell_size;
 
 % init sdf
@@ -19,10 +22,13 @@ disp('calculating signed distance field ...');
 field = signedDistanceField3D(dataset.map, dataset.cell_size);
 disp('calculating signed distance field done');
 
-sdf = SignedDistanceField(origin_point3, cell_size, size(field, 1), ...
-    size(field, 2), size(field, 3));
+% sdf = SignedDistanceField(origin_point3, cell_size, size(field, 1), ...
+%     size(field, 2), size(field, 3));
+
+sdf = SignedDistanceField_mex('new', origin, cell_size, size(field, 1), size(field, 2), size(field, 3));
 for z = 1:size(field, 3)
-    sdf.initFieldData(z-1, field(:,:,z)');
+    % sdf.initFieldData(z-1, field(:,:,z)');
+    SignedDistanceField_mex('initFieldData', sdf, z-1, field(:, :, z));
 end
 
 % plot 3D SDF
@@ -45,7 +51,8 @@ zlim([-0, 0.9])
 
 %% save SDF
 disp('saving sdf to .bin file...');
-sdf.saveSDF('WAMDeskDataset.bin');
+% sdf.saveSDF('WAMDeskDataset.bin');
+SignedDistanceField_mex('save', sdf, 'WAMDeskDataset_cereal.bin');
 
 % %% create the mesh for visualization
 % [X,Y,Z] = meshgrid(-10:1:20,-10:1:20,-10:1:20);
