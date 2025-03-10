223c4de7c6ec79a7a99fc6896ac5861e3370f824
zinuo <benjaminchang0731@gmail.com>
2025-03-09 22:05:44 -0400

    Updated time comparison

--- matlab_helpers/GVIMP-examples/2d_pR/Computation Time Figures/Evaluation_Time.m
+++ matlab_helpers/GVIMP-examples/2d_pR/Computation Time Figures/Evaluation_Time.m
@@ -2,7 +2,7 @@ clear; clc; close all;
 
 x0 = 500;
 y0 = 500;
-width = 1200;
+width = 1600;
 height = 900;
 
 set(groot, 'defaultTextInterpreter', 'latex');
@@ -32,17 +32,15 @@ CPU_avg = [0.3, 1.25, 3.55, 8.65, 16.65, 31.75, 96.35, 160.6];
 CPU_min = [0, 1, 3, 8, 14, 29, 92, 156];
 CPU_max = [1, 3, 4, 9, 22, 37, 102, 166];
 
-% gpu1 = plot(n_states, GPU_avg, '-o', 'MarkerSize', 16, 'LineWidth', 2.5, 'Color', 'g', ...
-%     'MarkerFaceColor', [0.5, 1, 0.5], 'MarkerEdgeColor', [0, 0.4, 0], ...
-%     'DisplayName', 'GPU Mean GH degree = 4');
-% fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.15, 'HandleVisibility', 'off');
-gpu1 = plot(n_states, GPU_avg, '-o', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'g', ...
-    'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 4$)');
-fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.15, 'HandleVisibility', 'off');
+ratio = (CPU_avg-GPU_avg)./CPU_avg
 
-cpu1 = plot(n_states, CPU_avg, '-o', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'r', ...
-    'MarkerEdgeColor', [0.35, 0, 0], 'DisplayName', 'CPU ($k_q = 4$)');
-fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.15, 'HandleVisibility', 'off');
+gpu1 = plot(n_states, GPU_avg, '-o', 'MarkerSize', 14, 'LineWidth', 3, 'Color', 'g', ...
+    'MarkerFaceColor', '[0.5, 1, 0.5]', 'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 4$)');
+% fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.15, 'HandleVisibility', 'off');
+
+cpu1 = plot(n_states, CPU_avg, '-o', 'MarkerSize', 14, 'LineWidth', 3, 'Color', 'r', ...
+    'MarkerFaceColor', '[1, 0.75, 0.8]', 'MarkerEdgeColor', [0.35, 0, 0], 'DisplayName', 'CPU ($k_q = 4$)');
+% fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.15, 'HandleVisibility', 'off');
 
 
 %% 2dpR GH Degree = 10
@@ -59,13 +57,15 @@ CPU_avg = [2.15, 4.2, 9.8, 22.35, 34.35, 64.45, 147.4, 211.9];
 CPU_min = [2, 4, 8, 21, 31, 59, 142, 202];
 CPU_max = [3, 5, 11, 27, 37, 72, 154, 222];
 
-gpu2 = plot(n_states, GPU_avg, '--x', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'g', ...
-    'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 10$)');
-fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+ratio = (CPU_avg-GPU_avg)./CPU_avg
 
-cpu2 = plot(n_states, CPU_avg, '--x', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'r', ...
+gpu2 = plot(n_states, GPU_avg, '--x', 'MarkerSize', 16, 'LineWidth', 3, 'Color', 'g', ...
+    'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 10$)');
+% fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+
+cpu2 = plot(n_states, CPU_avg, '--x', 'MarkerSize', 16, 'LineWidth', 3, 'Color', 'r', ...
     'MarkerEdgeColor', [0.35, 0, 0], 'DisplayName', 'CPU ($k_q = 10$)');
-fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+% fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
 
 
 %% 2dpR GH Degree = 20
@@ -81,14 +81,16 @@ CPU_avg = [13.55, 26.3, 58.6, 106.3, 163.25, 270.75, 442.55, 596.45];
 CPU_min = [11, 23, 50, 102, 158, 258, 420, 586];
 CPU_max = [17, 33, 69, 112, 179, 282, 464, 616];
 
-% Uncomment the following lines to plot the GH Degree = 20 curves
-gpu3 = plot(n_states, GPU_avg, '-.d', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'g', ...
-    'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 20$)');
-fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+ratio = (CPU_avg-GPU_avg)./CPU_avg
 
-cpu3 = plot(n_states, CPU_avg, '-.d', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'r', ...
-    'MarkerEdgeColor', [0.35, 0, 0], 'DisplayName', 'CPU ($k_q = 20$)');
-fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+% Uncomment the following lines to plot the GH Degree = 20 curves
+gpu3 = plot(n_states, GPU_avg, '-.d', 'MarkerSize', 14, 'LineWidth', 3, 'Color', 'g', ...
+    'MarkerFaceColor', [0.5, 1, 0.5], 'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 20$)');
+% fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+
+cpu3 = plot(n_states, CPU_avg, '-.d', 'MarkerSize', 14, 'LineWidth', 3, 'Color', 'r', ...
+    'MarkerFaceColor', [1, 0.75, 0.8], 'MarkerEdgeColor', [0.35, 0, 0], 'DisplayName', 'CPU ($k_q = 20$)');
+% fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
 
 xlabel("Time Discretizations")
 ylabel("Cost Evaluation Time (ms)")
@@ -97,4 +99,4 @@ legend([gpu1 gpu2 gpu3 cpu1 cpu2 cpu3], 'Location', 'northwest');
 grid minor
 
 set(gcf, 'Toolbar', 'none')
-exportgraphics(gcf, '2dpR_1_updated.pdf', 'ContentType', 'vector', 'BackgroundColor', 'none');
+% exportgraphics(gcf, '2dpR_1_updated.pdf', 'ContentType', 'vector', 'BackgroundColor', 'none');
--- matlab_helpers/GVIMP-examples/3d_pR/Evaluation_time.m
+++ matlab_helpers/GVIMP-examples/3d_pR/Evaluation_time.m
@@ -2,7 +2,7 @@ clear; clc; close all;
 
 x0 = 500;
 y0 = 500;
-width = 1200;
+width = 1600
 height = 900;
 
 set(groot, 'defaultTextInterpreter', 'latex');
@@ -32,13 +32,15 @@ CPU_avg = [2.2, 2.95, 7, 17.35, 29.35, 95.6, 179.8, 291.85];
 CPU_min = [1, 2, 6, 15, 26, 92, 173, 282];
 CPU_max = [8, 4, 11, 21, 32, 101, 190, 303];
 
+ratio = (CPU_avg-GPU_avg)./CPU_avg
+
 gpu1 = plot(n_states, GPU_avg, '-o', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'g', ...
-    'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 4$)');
-fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.15, 'HandleVisibility', 'off');
+    'MarkerFaceColor', [0.5, 1, 0.5], 'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 4$)');
+% fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.15, 'HandleVisibility', 'off');
 
 cpu1 = plot(n_states, CPU_avg, '-o', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'r', ...
-    'MarkerEdgeColor', [0.35, 0, 0], 'DisplayName', 'CPU ($k_q = 4$)');
-fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.15, 'HandleVisibility', 'off');
+    'MarkerFaceColor', [1, 0.75, 0.8],  'MarkerEdgeColor', [0.35, 0, 0], 'DisplayName', 'CPU ($k_q = 4$)');
+% fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.15, 'HandleVisibility', 'off');
 
 %% 3dpR GH Degree = 10
 n_states = [25, 50, 100, 200, 300, 500, 750, 1000];
@@ -53,13 +55,15 @@ CPU_avg = [17.4, 33.85, 69.8, 140.9, 209.25, 384.7, 598.65, 852];
 CPU_min = [15, 31, 65, 134, 196, 373, 576, 833];
 CPU_max = [20, 38, 75, 162, 221, 413, 631, 866];
 
-gpu2 = plot(n_states, GPU_avg, '--x', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'g', ...
-    'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 10$)');
-fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+ratio = (CPU_avg-GPU_avg)./CPU_avg
 
-cpu2 = plot(n_states, CPU_avg, '--x', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'r', ...
+gpu2 = plot(n_states, GPU_avg, '--x', 'MarkerSize', 16, 'LineWidth', 2.5, 'Color', 'g', ...
+    'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 10$)');
+% fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+
+cpu2 = plot(n_states, CPU_avg, '--x', 'MarkerSize', 16, 'LineWidth', 2.5, 'Color', 'r', ...
     'MarkerEdgeColor', [0.35, 0, 0], 'DisplayName', 'CPU ($k_q = 10$)');
-fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+% fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
 
 %% 3dpR GH Degree = 16
 n_states = [25, 50, 100, 200, 300, 500, 750, 1000];
@@ -74,19 +78,22 @@ CPU_avg = [142.15, 264.15, 542.75, 1022.55, 1605.4, 2620.85, 3891.75, 5246.3];
 CPU_min = [131, 251, 527, 995, 1562, 2560, 3809, 5127];
 CPU_max = [156, 287, 561, 1046, 1680, 2667, 3972, 5441];
 
+ratio = (CPU_avg-GPU_avg)./CPU_avg
+
 gpu3 = plot(n_states, GPU_avg, '-.d', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'g', ...
-    'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 16$)');
-fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+    'MarkerFaceColor', [0.5, 1, 0.5], 'MarkerEdgeColor', [0, 0.35, 0], 'DisplayName', 'GPU ($k_q = 16$)');
+% fill([n_states, fliplr(n_states)], [GPU_min, fliplr(GPU_max)], 'g', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
 
 cpu3 = plot(n_states, CPU_avg, '-.d', 'MarkerSize', 14, 'LineWidth', 2.5, 'Color', 'r', ...
-    'MarkerEdgeColor', [0.35, 0, 0], 'DisplayName', 'CPU ($k_q = 16$)');
-fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
+    'MarkerFaceColor', [1, 0.75, 0.8], 'MarkerEdgeColor', [0.35, 0, 0], 'DisplayName', 'CPU ($k_q = 16$)');
+% fill([n_states, fliplr(n_states)], [CPU_min, fliplr(CPU_max)], 'r', 'FaceAlpha', 0.2, 'HandleVisibility', 'off');
 
 xlabel("Time Discretizations")
 ylabel("Cost Evaluation Time (ms)")
 legend([gpu1, gpu2, gpu3, cpu1, cpu2, cpu3], 'Location', 'northwest');
 % legend([gpu1, gpu2, gpu3, cpu1, cpu2], 'Location', 'northwest');
 grid minor
+% ylim([0, 5500])
 
-set(gcf, 'Toolbar', 'none')
-exportgraphics(gcf, '3dpR_1_updated.pdf', 'ContentType', 'vector', 'BackgroundColor', 'none');
+% set(gcf, 'Toolbar', 'none')
+% exportgraphics(gcf, '3dpR_1_updated.pdf', 'ContentType', 'vector', 'BackgroundColor', 'none');
